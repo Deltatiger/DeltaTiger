fdce37743c8aedd69b549d61c7458e5be141b736
DeltaTiger <deltatiger94@gmail.com>
2012-06-18 07:25:57 +0530

    Trying to sync the latest

--- admin/index.php
+++ admin/index.php
@@ -2,8 +2,6 @@
 	//This is the admin index page.
 	include '../includes/common.php';
 	
-	header('Location:adminlogin.php');
-	
-	$template->set_page_template('admin_index');
+	$template->set_page_template('admin_index', 'Admin HQ');
 	
 ?>
--- cache/cached_files/cache.Enigma.admin_index.php
+++ cache/cached_files/cache.Enigma.admin_index.php
@@ -1,11 +1,37 @@
-<div class="adminMainBody">
-	<div class="adminTopNav">
-		This is the top nav bar
-	</div>
-	<div class="adminLeftNav">
-		This is the left Nav bar.
-	</div>
-	<div class="adminBody">
-		This is the main body with content
-	</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo (isset($this->templateVars['TITLEHERE'])) ? $this->templateVars['TITLEHERE'] : ''; ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="/Deltatiger/templates/Enigma/admin_enigma.css" />
+		<script type="text/javascript" src="/Deltatiger/templates/Enigma/ajax/ajax.js" ></script>
+	</head>
+	<body>
+		<div id="header">
+            <img src="/Deltatiger/templates/Enigma/images/logo.jpg" width =100% height = 150px />
+        </div>
+<div id="navbar">
+	<ul>
+		<li> Admin - Index </li>
+		<li> Statistics </li>
+		<li> Portal </li>
+	</ul>
 </div>
+
+<div id="body">
+		<div id="adminLeftNavBar">
+			This is the left nav bar.
+		</div>
+		<div id="adminRightMainBar">
+			This is the right main bar.
+		</div>
+</div>
+
+<!--
+    This page is the footer and the final template of the page.
+-->
+<div id="footer">
+    The Page Was Compiled on  07 : 06 : 56 20126 06-Jun-2012.
+</div>
+</body>
+</html>
